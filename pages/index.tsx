1f46763235b92b169f1065b2d65e347b3f0c112d
fullvids3 <133580358+fullvids3@users.noreply.github.com>
2024-10-10 11:26:46 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Vercel Trick By <a href="https://asiantv.wiki/">AsianTv</a>
+          Welcome to Vercel Trick By <a href="https://pinoytvhd.org/">PinoyTvHD</a>
         </h1>
 
         <p className={styles.description}>
@@ -22,32 +22,7 @@ const Home: NextPage = () => {
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://alcashzone.com/category/adsterra-earning/" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Here you can find how to earn money from Adsterra .</p>
-          </a>
-
-          <a href="https://alcashzone.com/category/adsense-approval/" className={styles.card}>
-            <h2>Master In AdSense Approval &rarr;</h2>
-            <p>Here you can find the of how you become master in AdSense approval!</p>
-          </a>
-
-          <a
-            href="https://alcashzone.com/category/blogging/"
-            className={styles.card}
-          >
-            <h2>Blogging &rarr;</h2>
-            <p>Here you can find the source to become master in blogging</p>
-          </a>
-
-          <a
-            href="https://newstick.us"
-            className={styles.card}
-          >
-            <h2>News &rarr;</h2>
-            <p>
-              Here you can find showviz news and other national and internation news
+       
             </p>
           </a>
         </div>
